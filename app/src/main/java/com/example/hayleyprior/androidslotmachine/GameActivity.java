52f224586f8252ea089014f2860fe3a8118b7a59
hdpuk86 <hayley.d.prior@gmail.com>
2017-11-11 22:12:58 +0000

    adds player funds to start game

--- app/src/main/java/com/example/hayleyprior/androidslotmachine/GameActivity.java
+++ app/src/main/java/com/example/hayleyprior/androidslotmachine/GameActivity.java
@@ -1,13 +1,26 @@
 package com.example.hayleyprior.androidslotmachine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView playerFunds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        playerFunds = findViewById(R.id.playerFundsText);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        Integer money = extras.getInt("playerFunds");
+
+        playerFunds.setText(money.toString());
+
     }
 }
