cca1b3392c4ac60ebbeda05e100916d5afffd2b0
Neo <c.min.tseng@gmail.com>
2022-11-24 03:49:50 +0800

    migrate to AndroidX

--- app/src/main/java/com/example/hayleyprior/androidslotmachine/HomeActivity.java
+++ app/src/main/java/com/example/hayleyprior/androidslotmachine/HomeActivity.java
@@ -1,11 +1,9 @@
 package com.example.hayleyprior.androidslotmachine;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
